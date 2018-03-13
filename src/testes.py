7c345d1621932a26004c4a01afca884931d424be
Bruno25galindo <36649085+Bruno25galindo@users.noreply.github.com>
2018-03-12 21:51:44 -0300

    corrigido 2x

--- src/testes.py
+++ src/testes.py
@@ -3,14 +3,14 @@ import sys
 erroInicializar = False
 jogo = jogovelha.inicializar()
 if len(jogo) != 3:
-  erroInicializar = True
+    erroInicializar = True
 else:
     for linha in jogo:
-      if len(linha) != 3:
-        erroInicializar = True
+        if len(linha) != 3:
+            erroInicializar = True
       else:
           for elemento in linha:
-            if elemento != '.':
+              if elemento != '.':
 erroInicializar = True
 if erroInicializar:
     sys.exit(1)
